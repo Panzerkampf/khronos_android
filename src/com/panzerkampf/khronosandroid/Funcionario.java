42cef929e9a14ed7ba2fa273745c2bb39870a1f7
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-08-25 21:56:06 -0300

    Criado a entidade funcionario

---
+++ src/com/panzerkampf/khronosandroid/Funcionario.java
@@ -0,0 +1,64 @@
+package com.panzerkampf.khronosandroid;
+
+import java.io.Serializable;
+
+public class Funcionario implements Serializable
+{
+	private static final long serialVersionUID = -4348018189820590485L;
+	private int id;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String data_nascimento;
+	private String identificacao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(String data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
+	public String getIdentificacao() {
+		return identificacao;
+	}
+
+	public void setIdentificacao(String identificacao) {
+		this.identificacao = identificacao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+}
